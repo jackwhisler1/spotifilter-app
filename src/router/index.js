c96443e01dd2324bf57ef08fdbfe14072f28ba2d
jack <whisler1@gmail.com>
2021-10-27 20:47:39 -0500

    init

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 Vue.use(VueRouter);
 
