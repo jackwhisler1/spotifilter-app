8420a97e9e80cb8a0605a32a7a8c5091d1d8b748
jack <whisler1@gmail.com>
2021-11-01 14:44:47 -0500

    adds playlist edit action, fixes home page button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="home">
     <h1>Welcome to SpotiFilter</h1>
+
     <a
-      :href="`https://accounts.spotify.com/authorize?client_id=${api_key}&response_type=code&redirect_uri=http://localhost:8080/spotify/callback&scope=playlist-read-private playlist-modify-private user-read-private user-read-email user-library-modify playlist-modify-public`"
+      :href="`https://accounts.spotify.com/authorize?client_id=${apiKey}&response_type=code&redirect_uri=http://localhost:8080/spotify/callback&scope=playlist-read-private playlist-modify-private user-read-private user-read-email user-library-modify playlist-modify-public`"
     >
-      Buttin
+      <button>Authorize Spotifilter</button>
     </a>
-    <button v-on:click="authorize()">Authorize Spotifilter</button>
     <div v-for="playlist in playlists" v-bind:key="playlist.id">
       <h2>{{ playlist.name }}</h2>
       <p>Total Tracks: {{ playlist.tracks.total }}</p>
@@ -26,22 +26,13 @@ export default {
   data: function () {
     return {
       playlists: [],
-      api_key: process.env.VUE_APP_SPOTIFY_API,
+      apiKey: process.env.VUE_APP_SPOTIFY_API,
     };
   },
   created: function () {
     this.playlistsIndex();
   },
   methods: {
-    authorize: function () {
-      axios
-        .get(
-          `https://accounts.spotify.com/authorize?client_id=${process.env.VUE_APP_SPOTIFY_API}&response_type=code&redirect_uri=http://localhost:8080/spotify/callback&scope=playlist-read-private playlist-modify-private user-read-private user-read-email user-library-modify playlist-modify-public`
-        )
-        .then((response) => {
-          console.log(response);
-        });
-    },
     playlistsIndex: function () {
       axios.get("/playlists").then((response) => {
         console.log(response.data);
--- src/views/PlaylistsShow1.vue
+++ src/views/PlaylistsShow1.vue
@@ -2,19 +2,33 @@
   <div class="playlists-show">
     <span>
       <button v-on:click="createPlaylist()">Create Filtered Playlist</button>
-
       <router-link to="/">
         <button>Back</button>
       </router-link>
-      <button v-on:click="deletePlaylist()">
+      <button v-if="playlistCreator === userId" v-on:click="deletePlaylist()">
         Delete Playlist
         <FlashMessage></FlashMessage>
       </button>
+      <button v-on:click="isShow = !isShow">Edit Playlist Details</button>
+      <form v-if="isShow" method="dialog">
+        <div>
+          Playlist Title
+          <input type="text" v-model="editPlaylistParams.name" />
+        </div>
+        <div>
+          Description:
+          <input type="text" v-model="editPlaylistParams.description" />
+        </div>
+        <button v-on:click="editPlaylist()">Submit</button>
+        <button>Close</button>
+      </form>
     </span>
+
     <h1>{{ playlist.name }}</h1>
+    <p>{{ playlist.description }}</p>
     <div v-for="track in tracks" v-bind:key="track.id">
       <div v-if="track.track.album.images[0]">
-        <h2>{{ track["track"]["name"] }} by {{ track["track"]["album"]["artists"][0]["name"] }}</h2>
+        <h3>"{{ track["track"]["name"] }}" by {{ track["track"]["album"]["artists"][0]["name"] }}</h3>
         <p>{{ track["track"]["album"]["name"] }}</p>
         <img v-bind:src="track.track.album.images[0].url" v-bind:alt="track.track.album.images" />
       </div>
@@ -24,7 +38,7 @@
 
 <style>
 img {
-  max-width: 10%;
+  max-width: 20%;
 }
 </style>
 
@@ -36,10 +50,18 @@ export default {
       playlist: {},
       tracks: [],
       newPlaylistParams: {},
+      userId: "",
+      playlistCreator: "",
+      editPlaylistParams: {
+        name: "",
+        description: "",
+      },
+      isShow: false,
     };
   },
   created: function () {
     this.showPlaylist();
+    this.getUserId();
   },
   methods: {
     showPlaylist: function () {
@@ -47,6 +69,7 @@ export default {
         console.log("playlist show", response.data);
         this.playlist = response.data;
         this.tracks = response.data.tracks.items;
+        this.playlistCreator = response.data.owner.id;
       });
     },
     createPlaylist: function () {
@@ -61,6 +84,22 @@ export default {
           console.log("playlists create error", error.response);
         });
     },
+    editPlaylist: function () {
+      axios
+        .patch(`/playlists/${this.$route.params.id}`, this.editPlaylistParams)
+        .then((response) => {
+          console.log("playlists edit", response);
+          this.showPlaylist();
+          this.editPlaylistParams = {
+            name: "",
+            description: "",
+          };
+          this.isShow = !this.isShow;
+        })
+        .catch((error) => {
+          console.log("playlists edit error", error.response);
+        });
+    },
     deletePlaylist: function () {
       axios.delete(`/playlists/${this.$route.params.id}`).then(
         this.flashMessage.success({
@@ -69,6 +108,17 @@ export default {
         })
       );
       setTimeout(() => this.$router.push("/"), 1200);
+    },
+    getUserId: function () {
+      axios
+        .get(`/spotify_usernames/${localStorage.getItem("user_id")}`)
+        .then((response) => {
+          console.log(response.data);
+          this.userId = response.data.id;
+        })
+        .catch((error) => {
+          console.log("username show error", error.response);
+        });
     },
   },
 };
