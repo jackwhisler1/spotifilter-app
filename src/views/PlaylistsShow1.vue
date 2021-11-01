c87d56532629fc3bc4aec138d221843d90b6bb77
jack <whisler1@gmail.com>
2021-10-31 21:24:28 -0500

    adds song title, artist, album, artwork and condition if no song title exists

--- src/views/PlaylistsShow1.vue
+++ src/views/PlaylistsShow1.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="playlists-show">
-    <!-- <h1>Show Page for {{  }}</h1> -->
-    <h1>Testing</h1>
+    <div v-for="track in tracks" v-bind:key="track.id">
+      <div v-if="track.track.album.images[0]">
+        <h2>{{ track["track"]["name"] }} by {{ track["track"]["album"]["artists"][0]["name"] }}</h2>
+        <p>{{ track["track"]["album"]["name"] }}</p>
+        <img v-bind:src="track.track.album.images[0].url" v-bind:alt="track.track.album.images" />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -13,15 +18,19 @@ export default {
   data: function () {
     return {
       playlist: {},
+      tracks: [],
       newPlaylistParams: {},
     };
   },
-  created: function () {},
+  created: function () {
+    this.showPlaylist();
+  },
   methods: {
     showPlaylist: function () {
       axios.get(`/playlists/${this.$route.params.id}`).then((response) => {
-        console.log("movies show", response);
+        console.log("playlist show", response.data);
         this.playlist = response.data;
+        this.tracks = response.data.items;
       });
     },
     createPlaylist: function () {
