809458f6651b38c113cb502a4cd0cf976d1a71fe
jack <whisler1@gmail.com>
2021-11-17 18:08:54 -0600

    debug

--- src/App.vue
+++ src/App.vue
@@ -209,6 +209,7 @@ export default {
     getUserId: function () {
       console.log(localStorage.user_id);
       this.userId = localStorage.user_id;
+      console.log("user id", this.userId);
       return this.userId;
     },
   },
